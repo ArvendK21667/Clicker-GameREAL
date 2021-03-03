6ea2e9dd41cb23f1702c53f432f18493a170a674
Arvend Karunagaran <arvend.karunagaran@studytafensw.edu.au>
2021-03-04 02:53:56 +1100

    update 1

--- Assets/IncomeGenerator.cs
+++ Assets/IncomeGenerator.cs
@@ -1,50 +1,50 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UI;
-//using TMPro;
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+//using UnityEngine.UI;
+////using TMPro;
 
 
-public class HealthManagers : MonoBehaviour
-{
+//public class HealthManagers : MonoBehaviour
+//{
 
-    public float health;
-    public float maxhealth;
-    public Button click;
-    //  public TextMeshProUGUI number;
+//    public float health;
+//    public float maxhealth;
+//    public Button click;
+//    //  public TextMeshProUGUI number;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        health = maxhealth;
-        click.fillAmount = 1;
-    }
+//    // Start is called before the first frame update
+//    void Start()
+//    {
+//        health = maxhealth;
+//        click.fillAmount = 1;
+//    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetKeyDown("b"))
-        {
-            health = health - 5;
-            if (health < 0)
-            {
-                health = 0;
-            }
-            float fraction = health / maxhealth;
-            click.fillAmount = fraction;
-            //number.text = health + "/" + maxhealth;
-        }
+//    // Update is called once per frame
+//    void Update()
+//    {
+//        if (Input.GetKeyDown("b"))
+//        {
+//            health = health - 5;
+//            if (health < 0)
+//            {
+//                health = 0;
+//            }
+//            float fraction = health / maxhealth;
+//            click.fillAmount = fraction;
+//            //number.text = health + "/" + maxhealth;
+//        }
 
-        if (Input.GetKeyDown("n"))
-        {
-            health = health + 5;
-            if (health > 100)
-            {
-                health = 100;
-            }
-            float fraction = health / maxhealth;
-            click.fillAmount = fraction;
-            //number.text = health + "/" + maxhealth;
-        }
-    }
-}
+//        if (Input.GetKeyDown("n"))
+//        {
+//            health = health + 5;
+//            if (health > 100)
+//            {
+//                health = 100;
+//            }
+//            float fraction = health / maxhealth;
+//            click.fillAmount = fraction;
+//            //number.text = health + "/" + maxhealth;
+//        }
+//    }
+//}
