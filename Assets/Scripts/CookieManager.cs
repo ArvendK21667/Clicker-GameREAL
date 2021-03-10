b56b58be5f864f31c90432dcad80f8629c3fc705
Arvend Karunagaran <arvend.karunagaran@studytafensw.edu.au>
2021-03-10 16:39:07 +1100

    Update Week 5 extras

--- Assets/Scripts/CookieManager.cs
+++ Assets/Scripts/CookieManager.cs
@@ -4,25 +4,31 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 
+
 public class CookieManager : MonoBehaviour
 {
 
     [SerializeField] private float cookies = 0f;
     [SerializeField] private float cookiesPerClick = 0.1f;
+    [SerializeField] private float cookieOverTime = 0f;
     [SerializeField] private TMP_Text cookieText;
     [SerializeField] private TMP_Text upgradeText;
-
     [SerializeField] private Button upgradeButton;
-    
+    [Range(1f, 4f)]
+    public float upgradeMultiplayer = 2.4f;
 
-
+    private bool AutoClickisUnlocked = false;
     private float costToUpgrade = 5;
     private object imagecolor;
 
     public void Start()
     {
+
         UpdateCookieText();
         UpdateUpgradeText();
+        AutomaticCookiesText();
+        // AutomaticCookies();
+        StartCoroutine(AutomaticCookies());
     }
 
 
@@ -40,29 +46,30 @@ public class CookieManager : MonoBehaviour
         {
 
             cookies -= costToUpgrade;
-            cookiesPerClick += 0.1f;
-            costToUpgrade = costToUpgrade * 1.4f;
+            cookiesPerClick += 0.2f;
+            costToUpgrade = costToUpgrade * upgradeMultiplayer;
+            upgradeMultiplayer -= 0.01f;
             UpdateCookieText();
             UpdateUpgradeText();
 
         }
 
-        
+
     }
 
     private void UpdateUpgradeText()
     {
-        upgradeText.text = " Upgrade " +  (int) costToUpgrade;
+        upgradeText.text = " Upgrade " + (int)costToUpgrade;
 
         if (cookies >= costToUpgrade)
         {
-            upgradeButton.GetComponent<UnityEngine.UI.Image>().color = Color.white;
-            upgradeButton.interactable = true;
+            upgradeButton.GetComponent<UnityEngine.UI.Image>().color = Color.white; // if true turn white
+            upgradeButton.interactable = true; // if true u can click the button
         }
         else
         {
-            upgradeButton.GetComponent<UnityEngine.UI.Image>().color = Color.red;
-            upgradeButton.interactable = false;
+            upgradeButton.GetComponent<UnityEngine.UI.Image>().color = Color.red; // if false turn red
+            upgradeButton.interactable = false; // if false you cant click the button 
         }
     }
 
@@ -81,7 +88,7 @@ public class CookieManager : MonoBehaviour
                     cookieText.text = " One Cookies ";
                     break;
                 default:
-                    cookieText.text = (int) cookies + " Cookies ";
+                    cookieText.text = cookies.ToString("f2") + " Cookies ";
                     break;
             }
 
@@ -90,8 +97,28 @@ public class CookieManager : MonoBehaviour
         {
             Debug.LogWarning("Cookie Text not set");
         }
-        
-       
+
+
+    }
+
+    IEnumerator AutomaticCookies()
+    {
+
+
+        while (AutoClickisUnlocked == true)
+        {
+            Debug.Log("Test Tell me whats wrong");
+            yield return new WaitForSeconds(10);
+            cookies += cookieOverTime;
+
+        }
+
+    }
+
+    void AutomaticCookiesText()
+    {
+
+
     }
 
 }
